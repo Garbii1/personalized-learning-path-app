6b81b09eadfd071e3e45a7d560507eda212f00c8
Garbii1 <muhdgaruba@gmail.com>
2025-04-10 13:50:16 +0100

    update footer

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
     <footer className="bg-gray-800 text-gray-300 py-6 mt-12">
       <div className="container mx-auto px-4 text-center">
         <p>© {currentYear} LearnPath. All rights reserved.</p>
-        <p className="text-sm mt-1">Created as a Full-Stack Developer Portfolio Project.</p>
+        <p className="text-sm mt-1"></p>
         {/* Add links to GitHub, portfolio, etc. if desired */}
       </div>
     </footer>
